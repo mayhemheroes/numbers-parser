3b381b3c3970af1b4e8527fe393360e665927d56
Bailey Capuano <bcapuano@asu.edu>
2022-12-19 20:44:27 -0500

    Initial MayhemHeroes Integration

--- mayhem/fuzz_numbers.py
+++ mayhem/fuzz_numbers.py
@@ -3,20 +3,21 @@
 import atheris
 import sys
 import fuzz_helpers
-from numbers_parser.exceptions import NumbersError
 
 with atheris.instrument_imports(include=["numbers_parser"]):
     from numbers_parser import Document
 
+from numbers_parser.exceptions import NumbersError
+from zipfile import BadZipFile
+from zlib import error
 
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     try:
         with fdp.ConsumeTemporaryFile('.numbers', all_data=True, as_bytes=True) as numbers_path:
-            Document(numbers_path)
-    except NumbersError:
+            doc = Document(numbers_path)
+    except (NumbersError, OSError, BadZipFile, error, NotImplementedError, UnicodeDecodeError):
         return -1
-
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
